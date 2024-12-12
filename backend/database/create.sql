75be6206c4a7dd8f5daf795926ef98e7fe6e445e
Ewan RENÉ-DAGUET <ewan.rene-daguet@etu.univ-grenoble-alpes.fr>
2024-12-12 05:36:11 +0100

    Ajustement SQL

--- backend/database/create.sql
+++ backend/database/create.sql
@@ -1,7 +1,22 @@
+-- Création d'un utilisateur avec un mot de passe
+CREATE USER admin WITH PASSWORD 'admin';
+
+-- Création de la base de données et assignation d'un propriétaire
+CREATE DATABASE bibliotheque OWNER admin;
+
+-- Connexion à la base de données
+\c bibliotheque;
+
+-- Création de la table 'livres'
 CREATE TABLE livres (
-    titre varchar(255) NOT NULL,
-    auteur varchar(255) NOT NULL,
-    categorie varchar(255) NOT NULL,
-    date_parution Date,
-    PRIMARY KEY (titre, auteur)
+    id SERIAL PRIMARY KEY, -- Ajout d'une colonne ID unique auto-incrémentée
+    titre VARCHAR(255) NOT NULL,
+    auteur VARCHAR(255) NOT NULL,
+    categorie VARCHAR(255) NOT NULL,
+    date_parution DATE,
+    UNIQUE (titre, auteur) -- Garantie qu'un titre et un auteur ne se répètent pas
 );
+
+-- Assigner les privilèges à l'utilisateur admin
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin;
